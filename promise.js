de4c0eaf87d32a9a9d174cc8486ae82e713cd2a9
yangliulang <yang_liulang@126.com>
2020-08-09 17:40:05 +0800

    todo..

--- promise.js
+++ promise.js
@@ -1,4 +1,8 @@
 class Promise {
+  // todo....
+  static race() {}
+  static reject(){}
+  static resolve()
   static all(promises) {
     return new Promise((resolve, reject) => {
       const promisesArray = [];
